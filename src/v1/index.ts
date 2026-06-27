b313754f7b96cd8138cdcc8bf3241719c927cf85
Fluttershy <1759100+Fluttershy@users.noreply.github.com>
2026-06-27 17:05:33 -0400

    restructured to use a /v1 import

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
-import MarketplaceSchema from "./generated/marketplace.schema.js";
+import MarketplaceSchema from "./marketplace.schema.js";
 export type MarketplaceJson = z.infer<typeof MarketplaceSchema>;
+export type MarketplacePluginJson = MarketplaceJson['plugins'][number];
 export { MarketplaceSchema };
 
-import PluginSchema from './generated/plugin.schema.js';
+import PluginSchema from './plugin.schema.js';
 export type PluginJson = z.infer<typeof PluginSchema>;
 export { PluginSchema };
